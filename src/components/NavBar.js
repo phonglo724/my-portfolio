ead52d04cee7d90290b1130a52b3ab3bdd0d485e
Phong Lo <phonglo724@gmail.com>
2021-04-21 18:16:58 -0600

    add components and styled navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+export default function NavBar() {
+    return (
+        <header className="bg-yellow-400">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink 
+                        to="/" 
+                        exact 
+                        activeClassName="text-white"
+                        className="inflex-flex items-center py-6 px-3 mr-4 text-black-100 hover:text-gray-600 text-4xl font-bold cursive tracking-widest"
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink 
+                        to="/about"
+                        className="inflex-flex items-center py-6 px-3 my-3 text-black-100 hover:text-gray-600 text-2xl font-bold cursive tracking-widest"
+                    >
+                        About Me!
+                    </NavLink>
+                    <NavLink 
+                        to="/project"
+                        className="inflex-flex items-center py-6 px-3 my-3 text-black-100 hover:text-gray-600 text-2xl font-bold cursive tracking-widest"
+                    >
+                        My Projects
+                    </NavLink>
+                    <NavLink 
+                        to="/blog"
+                        className="inflex-flex items-center py-6 px-3 my-3 text-black-100 hover:text-gray-600 text-2xl font-bold cursive tracking-widest"
+                    >
+                        My Blogs
+                    </NavLink>
+                    <NavLink 
+                        to="/resume"
+                        className="inflex-flex items-center py-6 px-3 my-3 text-black-100 hover:text-gray-600 text-2xl font-bold cursive tracking-widest"
+                    >
+                        Resume
+                    </NavLink>
+                    <NavLink 
+                        to="/contact"
+                        className="inflex-flex items-center py-6 px-3 my-3 text-black-100 hover:text-gray-600 text-2xl font-bold cursive tracking-widest"
+                    >
+                        Contact
+                    </NavLink>
+                </nav>
+                <div className="inline-flex py-3 px-3 my-6">
+                    {/* <SocialIcon 
+                        url="https://linkedin.com/in/phong-lo" 
+                        className="mr-4" 
+                        target="_blank" 
+                        fgColor="#fff" 
+                        style={{ height: 35, width:35 }} 
+                    />
+                    <SocialIcon 
+                        url="https://github.com/phonglo724" 
+                        className="mr-4" 
+                        target="_blank" 
+                        fgColor="#fff" 
+                        style={{ height: 35, width:35 }} 
+                    />
+                    <SocialIcon 
+                        url="https://phonglo724.medium.com/" 
+                        className="mr-4" 
+                        target="_blank" 
+                        fgColor="#fff" 
+                        style={{ height: 35, width:35 }} 
+                    /> */}
+                </div>
+            </div>
+        </header>
+    )
+}
