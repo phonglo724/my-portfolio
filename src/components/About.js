e7f4dbe20c40ffbe01366ed180ff1f8e28e8bc0e
Phong Lo <phonglo724@gmail.com>
2021-06-09 12:23:37 -0600

    changes to about me component

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import sanityClient from '../client.js';
 import AboutMePhoto from '../images/about-me-photo.jpg';
+import BlockContent from '@sanity/block-content-to-react';
 
 export default function About() {
 
@@ -19,33 +20,21 @@ export default function About() {
 
     return (
         <main className="relative bg-gray-300 min-h-screen p-12">
-            <div className="relative grid grid-flow-col auto-cols-max">
+            <section className="bg-gray-900 rounded-lg shadow-2xl lg:flex p-20">
                 <img 
                     src={AboutMePhoto} 
                     alt="self" 
                     className="photo-front rounded-full self-portrait ml-12 mt-12" 
                 />
-                    <p className="pl-40 pt-20 about-text mt-12">
-                        <strong className="text-2xl">Phong Lo</strong>
-                        <br></br>
-                        <br></br>
-                        Hello! I'm a full-stack software developer located in Denver, CO. 
-                        <br></br>
-                        The main area of my expertise is React JS, Ruby on Rails, Ruby,
-                        <br></br>
-                        HTML, CSS, and JavaScript. 
-                        <br></br>
-                        <br></br>
-                        When I'm not coding, I enjoy spending time with my partner Edd
-                        <br></br>
-                        and our cat, Nymeria. Otherwise, most likely you'll find me by a 
-                        <br></br>
-                        beach, pool, or mountain.
-                        <br></br>
-                        <br></br>
-                        Let's build something special.
-                    </p>
-            </div>
+                <div className="text-lg flex flex-col justify-center">
+                    <h1 className="about-text text-6xl text-gray-100 mb-4">
+                        <strong className="text-2xl pl-8">Phong Lo</strong>
+                    </h1>
+                    <div className="prose lg:prose-xl text-gray-100 pl-8">
+                        <BlockContent blocks={author.bio} projectId="2dgwiew2" dataset="production" />
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
