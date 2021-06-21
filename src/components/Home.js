7f8b93e0fb8d19d4b83742d73e44b559c8dbcd3a
Phong Lo <phonglo724@gmail.com>
2021-06-20 23:41:59 -0600

    home title styling changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,8 +16,8 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-            <div className="relative grid md:col-span-2 justify-end lg:pt-32 px-6">
-                <h1 className="home-name sm:px-8 sm:my-14 sm:py-48 md:px-64 md:my-24 md:py-24">Nyob Zoo! I'm <span className="span-color">Phong Lo</span>. I am a full stack software engineer.</h1>
+            <div className="relative grid md:col-span-2 justify-end lg:pt-32 px-8">
+                <h1 className="home-name sm:px-8 sm:my-14 sm:py-48 md:px-22 md:my-24 md:py-32">Nyob Zoo! I'm <span className="span-color">Phong Lo</span>. I am a full stack software engineer.</h1>
             </div>
             </main>
         </div>
