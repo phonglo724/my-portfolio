f426b896300de6c79f06c9e14836677c8d7425e6
Phong Lo <phonglo724@gmail.com>
2021-08-25 12:46:13 -0600

    navbar and home changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,17 @@ import HmongWoman from "../images/hmong-woman.jpeg";
 
 export default function Home() {
     return (
-        <div className="grid grid-cols-1 sm:px-9 sm:gap-x-8">
-            <main className="home-background min-h-screen">
+        <div className="flex flex-col justify-center">
+            <div className="home-background min-h-screen">
                 <img 
                     src={HmongWoman} 
-                    alt="Hmong Pattern" 
-                    className="absolute object-cover w-min h-screen" 
+                    alt="Traditional Hmong Woman" 
+                    className="absolute object-cover w-min h-screen invisible md:visible lg:visible" 
                 />
-            <div className="relative md:text-right sm:text-right">
-                <h1 className="home-name sm:px-8 sm:my-14 sm:py-48 md:px-22 md:my-24">Nyob Zoo! I'm <span className="span-color">Phong Lo</span>. I am a full stack software engineer.</h1>
+                <div className="relative md:text-right sm:text-right">
+                    <h1 className="home-name sm:text">Nyob Zoo! I'm <span className="span-color">Phong Lo</span>. I am a full stack software engineer.</h1>
+                </div>
             </div>
-            </main>
         </div>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,66 +4,62 @@ import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
     return (
-        <header className="bg-gray-500">
-            <div className="container mx-auto flex justify-between">
-                <nav className="flex items-center justify-between">
-                    <NavLink 
-                        to="/" 
-                        exact 
-                        activeClassName="text-"
-                        className="inflex-flex items-center py-6 px-3 mr-4 text-blue-900 hover:text-gray-300 text-4xl font-bold cursive tracking-widest"
-                    >
-                        Home
-                    </NavLink>
-                    <NavLink 
-                        to="/about"
-                        className="inflex-flex items-center py-6 px-3 my-3 text-black-100 hover:text-gray-600 text-2xl font-bold cursive tracking-widest"
-                    >
-                        About Me
-                    </NavLink>
-                    <NavLink 
-                        to="/project"
-                        className="inflex-flex items-center py-6 px-3 my-3 text-black-100 hover:text-gray-600 text-2xl font-bold cursive tracking-widest"
-                    >
-                        My Projects
-                    </NavLink>
-                    <NavLink 
-                        to="/blog"
-                        className="inflex-flex items-center py-6 px-3 my-3 text-black-100 hover:text-gray-600 text-2xl font-bold cursive tracking-widest"
-                    >
-                        My Blogs
-                    </NavLink>
-                    <NavLink 
-                        to="/resume"
-                        className="inflex-flex items-center py-6 px-3 my-3 text-black-100 hover:text-gray-600 text-2xl font-bold cursive tracking-widest"
-                    >
-                        Resume
-                    </NavLink>
-                </nav>
-                <div className="inline-flex py-3 px-3 my-6">
-                    <SocialIcon 
-                        url="https://linkedin.com/in/phong-lo" 
-                        className="mr-4" 
-                        target="_blank" 
-                        fgColor="#fff" 
-                        style={{ height: 35, width:35 }} 
-                    />
-                    <SocialIcon 
-                        url="https://github.com/phonglo724" 
-                        className="mr-4" 
-                        target="_blank" 
-                        fgColor="#fff" 
-                        style={{ height: 35, width:35 }} 
-                    />
-                    <SocialIcon 
-                        url="https://twitter.com/PhongLo13" 
-                        className="mr-4" 
-                        target="_blank" 
-                        fgColor="#fff" 
-                        style={{ height: 35, width:35 }} 
-                    />
-                </div>
+        <div>
+            <div className="bg-gray-500 flex items-center justify-between mx-auto">
+                <NavLink 
+                    to="/" 
+                    exact 
+                    activeClassName="text-"
+                    className="inflex-flex items-center py-6 px-3 mr-4 text-blue-900 hover:text-gray-300 text-sm md:text-4xl font-bold cursive tracking-widest"
+                >
+                    Home
+                </NavLink>
+                <NavLink 
+                    to="/about"
+                    className="inflex-flex items-center py-6 px-3 my-3 text-black-100 hover:text-gray-600 text-sm md:text-2xl font-bold cursive tracking-widest"
+                >
+                    About Me
+                </NavLink>
+                <NavLink 
+                    to="/project"
+                    className="inflex-flex items-center py-6 px-3 my-3 text-black-100 hover:text-gray-600 text-sm md:text-2xl font-bold cursive tracking-widest"
+                >
+                    My Projects
+                </NavLink>
+                <NavLink 
+                    to="/blog"
+                    className="inflex-flex items-center py-6 px-3 my-3 text-black-100 hover:text-gray-600 text-sm md:text-2xl font-bold cursive tracking-widest"
+                >
+                    My Blogs
+                </NavLink>
+                <NavLink 
+                    to="/resume"
+                    className="inflex-flex items-center py-6 px-3 my-3 text-black-100 hover:text-gray-600 text-sm md:text-2xl font-bold cursive tracking-widest"
+                >
+                    Resume
+                </NavLink>
+                <SocialIcon 
+                    url="https://linkedin.com/in/phong-lo" 
+                    className="mr-1" 
+                    target="_blank" 
+                    fgColor="#fff" 
+                    style={{ height: 35, width:35 }} 
+                />
+                <SocialIcon 
+                    url="https://github.com/phonglo724" 
+                    className="mr-1" 
+                    target="_blank" 
+                    fgColor="#fff" 
+                    style={{ height: 35, width:35 }} 
+                />
+                <SocialIcon 
+                    url="https://twitter.com/PhongLo13" 
+                    className="mr-1" 
+                    target="_blank" 
+                    fgColor="#fff" 
+                    style={{ height: 35, width:35 }} 
+                />
             </div>
-        </header>
+        </div>
     )
 }
